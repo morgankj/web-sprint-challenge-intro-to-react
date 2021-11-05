dea4a8b1208d998d4c265cc086eeee4ee3b3511b
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-11-05 14:00:01 -0500

    Made some progress with styling - still need to get more info in details and finalize styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Character from './components/Character';
 import Details from './components/Details';
+import logo from './images/StarWarsLogo.png';
+
+const StyledHeader = styled.div 
+` width: 30%;
+  display: flex;
+  flex-direction: column;
+  margin: 2% auto 0;
+`
+
+const StyledLogo = styled.img 
+` width: 100%
+`
+
+const StyledCharacterListings = styled.div 
+` display: flex;
+  justify-content: space-evenly;
+  margin: 5% auto;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out the state properties here.
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should sync up with, if any.
   const [characterList, setCharacterList] = useState([]);
   const [currentCharacter, setCurrentCharacter] = useState(null);
 
@@ -31,8 +47,13 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      {characterList.map(character => <Character character={character} key={character.url} open={openDetails} characterId={characterList.indexOf(character)} />)}
+      <StyledHeader>
+        <StyledLogo src={logo} alt=""/> 
+        <h1>CHARACTERS</h1>
+      </StyledHeader>
+      <StyledCharacterListings>
+        {characterList.map(character => <Character character={character} key={character.url} open={openDetails} characterId={characterList.indexOf(character)} />)}
+      </StyledCharacterListings>
       {currentCharacter && <Details character={currentCharacter} close={closeDetails} />}
     </div>
   );
